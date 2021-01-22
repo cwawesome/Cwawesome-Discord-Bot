b10db0f64faa35668a8eb0f6f5e08418bdb6f506
cwawesome <77744243+cwawesome@users.noreply.github.com>
2021-01-22 13:51:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+client.once('ready', () => {
+    console.log('bot is online!');
+});
+
+
+client.on('message', async message => {
+
+    if (message.content === `${prefix}ping`){
+        message.channel.send('Pong!');
+    }else if (!element.id == "801285931868356658"){
+        if(member.deaf){
+            member.voice.setChannel('801285931868356658') 
+        }
+    }
+
+    
+    
+});
+
+
+
+
+
+client.login(token)
